5f67b8f467a4537270d38f54fa5dbb6e7daf1803
Ashwini Vishwasrao Zoman <104773713+ashwinizoman86@users.noreply.github.com>
2022-05-08 16:43:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #We'll ask our app to put the list of selected fruits into a variable called fruits_selected.
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) #after removing ['Avocado','Strawberries]
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set
-streamlit.dataframe(fruits_selected)
+streamlit.dataframe(fruits_to_show)
 
 #Put a picklist
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
